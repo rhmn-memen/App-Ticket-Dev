a9c31b35390760c03da565d987eec50613fcbb63
rahman <rahmanambiya17@gmail.com>
2023-02-26 13:16:10 +0700

    first commit

---
+++ app/Http/Controllers/MasterProjectController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MasterProject;
+use Illuminate\Http\Request;
+
+class MasterProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MasterProject  $masterProject
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MasterProject $masterProject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MasterProject  $masterProject
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MasterProject $masterProject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MasterProject  $masterProject
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MasterProject $masterProject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MasterProject  $masterProject
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MasterProject $masterProject)
+    {
+        //
+    }
+}
